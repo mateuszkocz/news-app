49ca618eb0419cdc3a653c5574c9e14f61778e8d
Mateusz Kocz <mateusz@kocz.pl>
2019-06-02 21:41:03 +0200

    Add a load more button

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,9 +1,15 @@
 import React, { useContext } from 'react'
+import styled from 'styled-components'
 import ArticlesList from '../components/ArticlesList'
 import Filters from '../components/Filters'
 import Header from '../components/Header/Header'
 import Page from '../components/Page'
 import { ArticlesContext } from '../state/ArticlesProvider'
+
+const MoreButton = styled.button`
+  margin: 2rem auto;
+  display: block;
+`
 
 function Index() {
   const {ids, articles, fetching, error} = useContext(ArticlesContext)
@@ -21,6 +27,7 @@ function Index() {
       {
         !fetching && !error && <ArticlesList articles={articlesList}/>
       }
+      <MoreButton onClick={() => alert('Not yet implemented')}>Show more</MoreButton>
     </Page>
   )
 }

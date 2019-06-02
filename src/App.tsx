e033708c2609098fb7d52c44f1d5c313dbaccb0e
Mateusz Kocz <mateusz@kocz.pl>
2019-06-02 16:46:03 +0200

    Add a basic structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
-import React from 'react';
+import React from 'react'
+import Routes from './Routes'
 
 const App: React.FC = () => {
   return (
-    <div/>
-  );
+    <Routes/>
+  )
 }
 
-export default App;
+export default App
---
+++ src/Routes.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
+import Index from './pages/Index'
+import Article from './pages/Article'
+
+function Routes() {
+  return (
+    <Router>
+      <Route path="/" exact component={Index}/>
+      <Route path="/article/:articleId/" component={Article}/>
+    </Router>
+  )
+}
+
+export default Routes
